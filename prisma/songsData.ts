7548c1c17e804471049f3396e1e4dc99c255d5ea
Yago Mackay <yagomackay@outlook.com>
2023-04-04 05:09:49 +1000

    prisma

---
+++ prisma/songsData.ts
@@ -0,0 +1,73 @@
+export const artistsData: {
+  name: string;
+  avatar: string;
+  songs: any[];
+}[] = [
+  {
+    name: 'Glitch',
+    avatar:
+      'https://images.pexels.com/photos/5015227/pexels-photo-5015227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    songs: [
+      {
+        name: 'Fermi Paradox',
+        duration: 235,
+
+        url: 'https://dl.dropboxusercontent.com/s/7xmpwvvek6szx5n/fermi-paradox.mp3?dl=0',
+      },
+    ],
+  },
+  {
+    name: 'Purple Cat',
+    avatar:
+      'https://thumbs.dreamstime.com/z/beaver-blood-moon-eclipse-total-eclipse-begins-as-full-moon-moves-shadow-earth-resulting-total-eclipse-261578273.jpg',
+    songs: [
+      {
+        name: 'Long Day',
+        duration: 185,
+        url: 'https://dl.dropboxusercontent.com/s/9h90r7ku3df5o9y/long-day.mp3?dl=0',
+      },
+    ],
+  },
+  {
+    name: 'Ben Sound',
+    avatar:
+      'https://images.pexels.com/photos/13428290/pexels-photo-13428290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    songs: [
+      {
+        name: 'The Elevator Bossa Nova',
+        duration: 238,
+        url: 'https://dl.dropboxusercontent.com/s/7dh5o3kfjcz0nh3/The-Elevator-Bossa-Nova.mp3?dl=0',
+      },
+    ],
+  },
+  {
+    name: 'LiQWYD',
+    avatar:
+      'https://images.pexels.com/photos/15007786/pexels-photo-15007786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    songs: [
+      {
+        name: 'Winter',
+        duration: 162,
+        url: 'https://dl.dropboxusercontent.com/s/tlx2zev0as500ki/winter.mp3?dl=0',
+      },
+    ],
+  },
+  {
+    name: 'FSM Team',
+    avatar:
+      'https://images.pexels.com/photos/14417008/pexels-photo-14417008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    songs: [
+      {
+        name: 'Eternal Springtime',
+        duration: 302,
+        url: 'https://dl.dropboxusercontent.com/s/92u8d427bz0b1t8/eternal-springtime.mp3?dl=0',
+      },
+      {
+        name: 'Astronaut in a Submarine',
+        duration: 239,
+        artist: 'FSM Team',
+        url: 'https://dl.dropboxusercontent.com/s/9b43fr6epbgji4f/astronaut-in-a-submarine.mp3?dl=0',
+      },
+    ],
+  },
+];
